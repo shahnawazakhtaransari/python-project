b22cc3af1d08c05b679a7a81e6d4c46545734a46
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-16 00:44:33 +0000

    game

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,5 +1,5 @@
-def sum(a,b,c):
-    return a+b+c
+def sum(a, b, c ):
+    return a + b + c
 
 def printBoard(xState, zState):
     zero = 'X' if xState[0] else ('O' if zState[0] else 0)
@@ -17,36 +17,36 @@ def printBoard(xState, zState):
     print(f"--|---|---")
     print(f"{six} | {seven} | {eight} ") 
 
-def checkwin(xstate,zstate):
-    wins=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+def checkWin(xState, zState):
+    wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for win in wins:
-        if(sum(xstate[win[0]],xstate[win[1]],xstate[win[2]])==3):
-            print("X won the match")
+        if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3):
+            print("X Won the match")
             return 1
-        if(sum(zstate[win[0]],zstate[win[1]],zstate[win[2]])==3):
-            print("O won the match")
+        if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3):
+            print("O Won the match")
             return 0
     return -1
-
+    
 if __name__ == "__main__":
-    xstate = [0,0,0,0,0,0,0,0,0]
-    zstate = [0,0,0,0,0,0,0,0,0]
-    turn=1 #1 for x and 0 for O
-    print("Welcome to Tic-tac-toe")
+    xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    zState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    turn = 1 # 1 for X and 0 for O
+    print("Welcome to Tic Tac Toe")
     while(True):
-        printBoard(xstate,zstate)
-        if(turn==1):
-           print("x's chance")
-           value = int(input("please enter a value: "))
-           xstate[value]=1
+        printBoard(xState, zState)
+        if(turn == 1):
+            print("X's Chance")
+            value = int(input("Please enter a value: "))
+            xState[value] = 1
         else:
-           print("O's chance")
-           value = int(input("please enter a value:1 "))  
-           zstate[value]=1
-           cwin=checkwin(xstate,zstate)
-           if(cwin!=-1):
-               print("Match Over! ")
-               break
-           turn=1-turn
-
+            print("O's Chance")
+            value = int(input("Please enter a value: "))
+            zState[value] = 1
+        cwin = checkWin(xState, zState)
+        if(cwin != -1):
+            printBoard(xState, zState)
+            print("Match over")
+            break
         
+        turn = 1 - turn
